1fa5ff91624f804ea0d9e150b4d739924b893185
Omar Silva <omarsilva@outlook.com>
2020-08-09 01:44:56 -0500

    Added api and make a cll using fetch

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Events Listing</h1>
-    <EventCard />
+    <EventCard v-for="event in events" v-bind:key="event.id" v-bind:event="event" />
   </div>
 </template>
 
@@ -12,6 +12,25 @@ import EventCard from "../components/EventCard.vue";
 export default {
   components: {
     EventCard
+  },
+  data: function() {
+    return {
+      events: []
+    };
+  },
+  created: function() {
+    fetch("http://localhost:3000/events")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(
+        function(json) {
+          this.events = json;
+        }.bind(this)
+      )
+      .catch(function(error) {
+        console.log("There was an error: " + error.message);
+      });
   }
 };
 </script>
